a1bd6535756fd061d16c0683b1273c2b68bf1ac2
Sicong Huang <huangs33@uw.edu>
2018-08-26 21:45:20 +0800

    modified load_data() added load_object()

--- data_utils.py
+++ data_utils.py
@@ -1,9 +1,10 @@
 import numpy as np
+import pickle
 
 # load indexed sentence data
-def load_data(which_data):
-    with np.load('data/' + which_data + '.npz') as f:
-        data = f[which_data].astype(np.int32)
+def load_data(file, name, type):
+    with np.load('data/' + file) as f:
+        data = f[name].astype(type)
     return data
 
 # a function to trim the first axis of data to fit the given batch size
@@ -11,3 +12,8 @@ def fit_batch(data, batch):
     data_points = data.shape[0]
     remainder = data_points % batch
     return data[:data_points - remainder]
+
+def load_object(file):
+    with open('data/' + file, 'rb') as f:
+        obj = pickle.load(f)
+    return obj
