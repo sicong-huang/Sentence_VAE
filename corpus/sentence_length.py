5dd72d973a19ff58ecb5210c679ac41e007f8d50
wanwanaa <1551612415@qq.com>
2018-09-13 21:35:56 +0800

    add comment

--- corpus/sentence_length.py
+++ corpus/sentence_length.py
@@ -1,49 +1,14 @@
-# Calculate average sentence length
+# Sentence length histogram
+
 import matplotlib.pyplot as plt
-# sentence length(dict)
-'''length = dict()
-with open('data/train', encoding='utf-8') as f:
-    for line in f:
-        line = line.strip('\n').split(' ')
-        flag = length.get(len(line))
-        if flag is None:
-            length[len(line)] = 1
-        else:
-            length[len(line)] += 1
-# Scatter
-x_values = length.keys()
-y_values = length.values()
-plt.scatter(x_values, y_values, s=10)'''
-
-'''data = dict()
-for k, v in length.items():
-    key = k//5
-    flag = data.get(key)
-    if flag is None:
-        data[key] = v
-    else:
-        data[key] += v
-data = sorted(data.items(), key=lambda x: x[0], reverse=False)
-y = []
-for i in data:
-    y.append(i[1])
-# histogram
-plt.bar(range(len(y)), y)'''
-
-import numpy as np
-import math
 data = []
-with open('data/train', encoding='utf-8') as f:
+with open('../raw_data/ptb/train', encoding='utf-8') as f:
     for line in f:
         line = line.strip('\n').split(' ')
         data.append(len(line))
-mean = sum(data)/len(data)
-var = math.sqrt(np.var(data))
-print(round(mean+var))
-
-'''plt.hist(data, bins=40, normed=0, facecolor="blue", edgecolor="black", alpha=0.7)
+plt.hist(data, bins=40, normed=0, facecolor="blue", edgecolor="black", alpha=0.7)
 plt.title('sentcece_length', fontsize=12)
 plt.xlabel('length', fontsize=10)
 plt.ylabel('num', fontsize=10)
 plt.tick_params(axis='both', which='major', labelsize=7)
-plt.show()'''
+plt.show()
--- utils.py
+++ utils.py
@@ -7,15 +7,18 @@ def load_data(file, name, type):
         data = f[name].astype(type)
     return data
 
+
 # a function to trim the first axis of data to fit the given batch size
 def fit_batch(data, batch):
     data_points = data.shape[0]
     remainder = data_points % batch
     return data[:data_points - remainder]
 
+
 def save_object(obj, path):
     with open(path, 'wb') as f:
         pickle.dump(obj, f)
+
 
 def load_object(path):
     with open(path, 'rb') as f:
